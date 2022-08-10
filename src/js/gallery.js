8917b053ce354d21b707680c22175f12f93b5ef2
NataliiaMax <105719575+NataliiaMax@users.noreply.github.com>
2022-08-10 09:41:32 +0300

    create gallery-js

---
+++ src/js/gallery.js
@@ -0,0 +1,10 @@
+import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.min.css';
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    close: true,
+    docClose: true,
+  throttleInterval: 100,
+});
