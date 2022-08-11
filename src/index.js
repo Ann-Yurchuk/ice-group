7e1cf4a254d8dcb04eef4b4fa6867a57b30e8d3a
SashaSerh <101942102+SashaSerh@users.noreply.github.com>
2022-08-11 14:11:52 +0300

    added animation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './js/slick-slider'
 import './js/menu'
-import './js/gallery'
+// import './js/gallery'
 import './js/modal'
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,5 +1,5 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-const gallery = new SimpleLightbox('.gallery a', {
-    captionDelay: 250,
-      });
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+// const gallery = new SimpleLightbox('.gallery a', {
+//     captionDelay: 250,
+//       });
