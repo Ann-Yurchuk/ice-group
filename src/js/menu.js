cacda83929a78457222873a11b03cade6030c8c0
Ann-Yurchuk <105579383+Ann-Yurchuk@users.noreply.github.com>
2022-08-11 11:05:09 +0300

    update menu js

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,22 +1,15 @@
 (() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
+    const body = document.querySelector("[data-body]");
 
-  const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
+    menuBtnRef.addEventListener("click", () => {
+        const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  };
+        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
 
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
-
-
+        mobileMenuRef.classList.toggle("is-open");
+        body.classList.toggle("no-scroll");
+    });
 })();
