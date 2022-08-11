54f79a8f861e3c95ad6e3ab07329df6223e3fba9
Ann-Yurchuk <105579383+Ann-Yurchuk@users.noreply.github.com>
2022-08-11 19:16:09 +0300

    add

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,5 +1,7 @@
+   
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 const gallery = new SimpleLightbox('.gallery a', {
-    captionDelay: 250,
-      });
+  captionsData: 'alt',
+  captionDelay: 250,
+});
